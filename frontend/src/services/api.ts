dbaeb5f553d292fd8479466b8e6ece1e752cf9ea
yashanandd <yash983747@gmail.com>
2026-05-15 02:35:05 +0530

    Added requirements file

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:8000/api/v1';
+const API_URL = "https://ai-resume-analyzer-backend.onrender.com";
 
 export const api = axios.create({
   baseURL: API_URL,
